9d8ff1a997431692fe0d47169b92d15b81fd346e
mysteriousman <justmysteriousman@163.com>
2023-07-23 22:20:46 +0800

    button for operating

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,17 +13,32 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.grey[800],
+        child: const Icon(Icons.add),
+      ),
       body: SafeArea(
         child: SliderDrawer(
-          appBar: const SliderAppBar(
-              appBarHeight: 50,
-              appBarColor: Colors.white,
-              appBarPadding: EdgeInsets.zero,
-              title: Text("Pocket Note",
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w700,
-                      wordSpacing: 8))),
+          appBar: SliderAppBar(
+            appBarHeight: 50,
+            appBarColor: Colors.white,
+            appBarPadding: EdgeInsets.zero,
+            title: const Text("Pocket Note",
+                style: TextStyle(
+                    fontSize: 22, fontWeight: FontWeight.w700, wordSpacing: 8)),
+            trailing: TextButton(
+              onPressed: () {},
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.add_alert_sharp,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            ),
+          ),
           slider: const Setting(),
           child: Container(color: Colors.white),
         ),
